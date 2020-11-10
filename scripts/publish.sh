d871c2c3e12eeb8e2cd165be2310ba89f32b2c0d
ycren <ycren@freewheel.tv>
2020-11-10 18:28:16 +0800

    ref: polish the publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,12 +1,16 @@
-git stash
-git branch -D gh-pages
-git checkout gh-pages
-git stash pop
-echo 'yarn installing:'
-yarn
-echo 'building package:'
-yarn demo
-git add .
-git commit -m 'demo: publish demo' -n
-echo 'publishing...'
-git push origin gh-pages -f
+# !/bin/bash
+if [ -z "$(git status --porcelain)" ]; then 
+  git branch -D gh-pages
+  git checkout gh-pages
+  echo "\x1b[32mInfo: yarn installing...\x1b[39m"
+  yarn
+  echo "\x1b[32mInfo: building...\x1b[39m"
+  yarn demo
+  git add .
+  git commit -m 'demo: publish demo' -n
+  echo "\x1b[32mInfo: publishing...\x1b[39m"
+  git push origin gh-pages -f
+  git checkout master
+else
+  echo "\x1b[31mError: Please clean your workspace before publishing\x1b[39m"
+fi
