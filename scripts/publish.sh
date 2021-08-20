c91ba47e5403806393ceb5ea62151c1ea1e1c496
ycren <ycren@freewheel.tv>
2021-08-20 15:38:38 +0800

    fix: fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,8 @@
 # !/bin/bash
 if [ -z "$(git status --porcelain)" ]; then 
+  git push origin :gh-pages
   git branch -D gh-pages
-  git checkout gh-pages
+  git checkout -b gh-pages
   echo "\x1b[32mInfo: yarn installing...\x1b[39m"
   yarn
   echo "\x1b[32mInfo: building...\x1b[39m"
@@ -10,6 +11,10 @@ if [ -z "$(git status --porcelain)" ]; then
   git commit -m 'demo: publish demo' -n
   echo "\x1b[32mInfo: publishing...\x1b[39m"
   git push origin gh-pages -f
+
+  branch_name=publish_`date +%Y_%m_%d`
+  git checkout -b $branch_name
+  git push origin $branch_name -f
   git checkout master
 else
   echo "\x1b[31mError: Please clean your workspace before publishing\x1b[39m"
